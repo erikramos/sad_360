fb8b3b5fd3e11fc290d16e6a6af0424b64e3ff97
erikramos <erik.ramos@virgos.com.br>
2015-07-26 19:00:44 -0300

    Telas iniciais

---
+++ application/controllers/cavaliacao.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+include("cbase.php");
+
+class CAvaliacao extends cbase {
+
+	// Layout default utilizado pelo controlador.
+	public $layout = 'default';
+
+	//Titulo default.
+	public $title = '';
+
+	//Definindo os css default.
+	public $css = array('template');
+
+	//Carregando os js default.
+	public $js = array('template');
+
+	//metodo construtor do controller
+	public function __construct()
+   	{
+    	parent::__construct();
+   	}
+
+   	function index(){
+   		$this->listar();
+   	}
+
+	function listar(){
+
+		$this->layout = 'default';					//informa qual template utilizar para carregar a view dentro
+		$this->title = '::: SAD-360 :::';			//informa o titulo da pagina
+		$this->css = array('Template/template');	//informa o arquivo css a ser carregado com layout da pagina
+		$this->js = array('Avaliacao/listar');			//informa o arquivo js com scripts de execução da pagina
+		$this->load->view('Avaliacao/listar');			//carrega a view
+	}
+}
--- application/views/Template/menu_superior.php
+++ application/views/Template/menu_superior.php
@@ -14,16 +14,21 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href=<?php echo base_url("index.php/cpainel/") ?> >Painel</a></li>
-        <li><a href="#">Avalia&ccedil;&otilde;es</a></li>
-        <li class="dropdown">
+        <li <?php if(strpos(uri_string(), "cpainel") !== false) echo 'class="active"'; ?> ><a href=<?php echo base_url("index.php/cpainel/") ?> >Painel</a></li>
+        <li <?php if(strpos(uri_string(), "crelatorio") !== false) echo 'class="active"'; ?> ><a href=<?php echo base_url("index.php/crelatorio/listar") ?> >Relat&oacute;rio</a></li>
+
+        <li <?php if((strpos(uri_string(), "cavaliacao") !== false) ||
+                    (strpos(uri_string(), "cusuario") !== false) ||
+                    (strpos(uri_string(), "ccargo") !== false) ||
+                    (strpos(uri_string(), "cperfil") !== false)) echo 'class="active"'; ?> class="dropdown">
+
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cadastros <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Usu&aacute;rios</a></li>
-            <li><a href="#">Avalia&ccedil;&otilde;es</a></li>
-            <li><a href="#">Quest&otilde;es</a></li>
+            <li <?php if(strpos(uri_string(), "cavaliacao") !== false) echo 'class="active"'; ?> ><a href=<?php echo base_url("index.php/cavaliacao/listar") ?> >Avalia&ccedil;&otilde;es</a></li>
             <li class="divider"></li>
-            <li><a href="#">Perfis</a></li>
+            <li <?php if(strpos(uri_string(), "cusuario") !== false) echo 'class="active"'; ?> ><a href=<?php echo base_url("index.php/cusuario/listar") ?> >Usu&aacute;rios</a></li>
+            <li <?php if(strpos(uri_string(), "ccargo") !== false) echo 'class="active"'; ?> ><a href=<?php echo base_url("index.php/ccargo/listar") ?> >Cargos</a></li>
+            <li <?php if(strpos(uri_string(), "cperfil") !== false) echo 'class="active"'; ?> ><a href=<?php echo base_url("index.php/cperfil/listar") ?> >Perfis</a></li>
           </ul>
         </li>
       </ul>
